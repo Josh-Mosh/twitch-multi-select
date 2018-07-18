cdc6940357a8adc3ca278ee3d0eba42b11168fdc
Josh Gillespie <joshua.neal.gillespie@gmail.com>
2018-07-18 11:49:09 -0500

    add overlay and live stream overlays, add styles

--- action.js
+++ action.js
@@ -1,17 +1,53 @@
 $(document).ready(function() {
 
-  var counter = 0;
+  let selecting = false;
+  //
+  // const streams = $('.live-channel-card a.tw-interactive.tw-link');
+  // const streamsAlt = $('[data-a-target="preview-card-image-link"]');
+  // const streams3 = $('.live-channel-card');
 
-  var streams = $('.live-channel-card .tw-interactive.tw-link');
+  // $.each(streams, (index, stream) => {
+  //   console.log(stream, $(stream).attr('href'), $('[data-a-target="preview-card-image-link"]'));
+  // });
+  //
+  // $.each(streams3, (index, stream) => {
+  //   console.log(stream, $(stream).find('img'));
+  // });
 
-  let firstStream = streams[0];
+  // find selectable live streams
+  liveStreams = [];
+  for (var i = 0; i < 100; i++) {
+    let stream = $('[data-a-target="live-channel-card-' + i + '"]');
+    if (stream && stream.length) {
+      $(stream).addClass('selectable-stream');
+      liveStreams.push(stream);
+    }
+  }
+  // console.log('liveStreams ', liveStreams);
+  //
+  // $.each(liveStreams, (index, stream) => {
+  //   console.log($(stream), $(stream).find('[data-test-selector="preview-card-thumbnail__image-selector"]'));
+  // });
 
-  $.each(streams, (index, stream) => {
-    console.log('first ', $(stream).attr('href'));
-  })
+  function startSelecting() {
+    selecting = true;
+    if ($('.overlay').length === 0) {
+      $('<div class="overlay"></div>').prependTo('.root-scrollable__wrapper');
+    }
 
-  // alert('some thing happaitng here');
+    $.each(liveStreams, (index, stream) => {
+      console.log($(stream), $(stream).find('[data-test-selector="preview-card-thumbnail__image-selector"]'));
+      $('<a class="stream-overlay"></a>').prependTo(stream);
+    });
+  }
+
+  startSelecting();
+
+  // remove added classes and elements on overlay click
+  $('.overlay').on('click', function() {
+    $(this).remove();
+    $('.stream-overlay').remove();
+    $('.selectable-stream').removeClass('selectable-stream');
+  });
+
 });
-
-
-// alert('some thing happaitng 123');
